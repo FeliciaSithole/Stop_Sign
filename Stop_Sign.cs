da9327f6c971624e5b056f1626ed73ded5e5e235
Felicia Sithole <Felicia.Sithole@dynamicdna.co.za>
2025-12-05 12:23:02 +0200

    initial commit

---
+++ Stop_Sign.cs
@@ -0,0 +1,18 @@
+using System;
+
+class StopSign
+{
+    static void Main()
+{
+        // Print the stop sign top
+        Console.WriteLine("  XXXXXX");
+        Console.WriteLine(" X      X");
+        Console.WriteLine("X  STOP  X");
+        Console.WriteLine(" X      X");
+        Console.WriteLine("  XXXXXX");
+
+        {
+            Console.WriteLine("    X");
+        }
+    }
+}
